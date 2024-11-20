c754c5f1d1f057e8253c1d14e9e9a2a856341376
agathemczk <agathe.maciaszczyk@icloud.com>
2024-11-20 09:14:35 +0100

    first commit

--- tron-main/src/main/java/com/jad/tron/Sprite.java
+++ tron-main/src/main/java/com/jad/tron/Sprite.java
@@ -2,3 +2,6 @@ package com.jad.tron;
 
 public record Sprite(char symbol) {
 }
+
+// un record fabrique get, constructeur, hashcode, tout MAIS on ne peut pas modifier les valeurs à
+// à l'intérieur > utile pour des choses qu'on ne modofie pas après
